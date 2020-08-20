75db9ea38a3479d9379b970d625ce827a2deb3f0
optimusneo <jonhybridge@gmail.com>
2020-08-20 12:40:35 -0500

    Commit 3

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //habilitar la carpeta Public
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use('/', express.static(path.resolve(__dirname, '../public')));
 
 //Configuración global de rutas
 app.use(require('./routes/index'));
